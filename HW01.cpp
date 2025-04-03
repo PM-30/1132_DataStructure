ceda6d60cfde1571659ae7002a552565eb5eb7eb
PM-30 <t113310236@ntut.org.tw>
2025-04-03 19:38:26 +0800

    Update HW01.cpp

--- HW01.cpp
+++ HW01.cpp
@@ -14,12 +14,16 @@ int drawCard() {
 }
 
 // 初始化玩家
-void initializePlayer(Player* player, string name) {
-    player->name=name;
-    player->score=0;
+void initializePlayer(Player* player, string name) {//透過call by referemce的方式初始化
+    player->name=name;//初始化腳色身分
+    player->score=0;//初始化腳色的分數
+    int num = 0;
+    for(int i=0 ; i<2 ; i++){//發兩張牌初始化剛開始的分數
+        num = drawCard();
+        player->score += num;
+    }
 }
-// 顯示玩家分數
-void displayScore(Player* player) {
+void displayScore(Player* player) {// 顯示玩家分數
     cout<<"目前總分:"<<player->score<<endl;
 }
 // 玩家回合
@@ -29,10 +33,10 @@ void playerTurn(Player* player) {
         cout << "要抽牌嗎？(h = 抽, s = 停) : ";
         cin >> choice;
         if (choice == 'h') {
-            int num = drawCard() ;
-            player->score = player->score + num;
-            cout<<"抽到"<<num<<"點，" ;
-            displayScore(player);
+            int num = drawCard();//設定變數儲存抽出的數值
+            player->score = player->score + num;//使原本擁有的分數加上抽出的數值
+            cout<<"抽到"<<num<<"點，" ;//展示抽到的點數
+            displayScore(player);//展示加總後的總分
             cout<< player->score<<endl;
             if (player->score > 21) {
                 cout << player->name << " 爆掉了！遊戲結束。\n";
@@ -47,9 +51,9 @@ void playerTurn(Player* player) {
 // 莊家回合
 void dealerTurn(Player* dealer) {
     while (dealer->score < 17) { // 莊家至少要到 17 點
-        int newCard = drawCard();
-        dealer->score = dealer->score + newCard ; 
-        cout << "莊家抽到" << newCard << "點，目前總分: " << dealer->score << endl;
+        int newCard = drawCard();//設定變數儲存抽到的分數
+        dealer->score = dealer->score + newCard ; //抽到的分數加上原有的
+        cout << "莊家抽到" << newCard << "點，目前總分: " << dealer->score << endl;//展示總分
         if (dealer->score > 21) { // 如果莊家超過 21 點
             cout << "莊家爆了！玩家獲勝！" << endl;
             return; // 遊戲結束
